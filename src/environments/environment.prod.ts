eda0c482e580bfda5c6f9aff7829a4d5b6a48cbb
Weed@2021 <geargym-steroids@yandex.com>
2022-07-26 14:26:15 +0100

    api link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://natures-api.herokuapp.com/api/v1'
+  // API_URL: 'https://natures-api.herokuapp.com/api/v1'
+  API_URL: 'https://khalif-natures-backend.herokuapp.com/api/v1'
 };
