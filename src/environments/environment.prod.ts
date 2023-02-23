34df13ffea4fd3e76443642db0acf8ded762b396
high-green <82278956+high-green@users.noreply.github.com>
2023-02-23 08:38:32 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // API_URL: 'https://natures-api.herokuapp.com/api/v1'
-  API_URL: 'https://khalif-natures-backend.herokuapp.com/api/v1'
+  API_URL: 'https://organics-api.osc-fr1.scalingo.io/api/v1'
 };
