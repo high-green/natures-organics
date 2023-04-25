239126157a008f084b7aef70fcbf3460a68ed821
high-green <82278956+high-green@users.noreply.github.com>
2023-04-25 18:06:26 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // API_URL: 'https://natures-api.herokuapp.com/api/v1'
-  API_URL: 'https://organics-email-api.osc-fr1.scalingo.io/api/v1'
+  API_URL: 'https://colorado-frontend.osc-fr1.scalingo.io/api/v1'
 };
