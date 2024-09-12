6bb5a02f677be14b80b9ba74835060c14ccee0e7
high-green <82278956+high-green@users.noreply.github.com>
2024-09-12 14:48:17 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // API_URL: 'https://natures-api.herokuapp.com/api/v1'
-  API_URL: 'https://august-api.osc-fr1.scalingo.io/api/v1'
+  API_URL: 'https://september-api.osc-fr1.scalingo.io/api/v1'
 };
